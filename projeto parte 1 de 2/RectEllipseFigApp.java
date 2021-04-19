65ac045c13cf27847e7539f850e69e49e3eca1a6
Talita Ribeiro <57768101+Talitafr@users.noreply.github.com>
2021-04-19 08:10:15 -0300

    Add files via upload

--- projeto parte 1 de 2/RectEllipseFigApp.java
+++ projeto parte 1 de 2/RectEllipseFigApp.java
@@ -20,16 +20,31 @@ class RectEllipseFigApp {
     public static void main (String[] args) {
         ListFrame frame1 = new ListFrame();
         frame1.setVisible(true);
+        //JLayeredPane layeredPane = new JLayeredPane();
+        //layeredPane.setBounds(0,0,350,350);
+        //frame1.add(layeredPane);
+        
     }
 }
 
 class ListFrame extends JFrame {
-	ArrayList<Figure> figs = new ArrayList<Figure>();
+    ArrayList<Figure> figs = new ArrayList<Figure>();
     Random rand = new Random();
-    Figure focus = null;
+    Figure foco = null;
+    
+    public boolean selected;
+    Point antes;
+    
+    
+   
     
     
     ListFrame () {
+        
+        //Figure foco;
+        
+        
+       // this.focus = null;
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
@@ -41,9 +56,10 @@ class ListFrame extends JFrame {
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
+                    
                 	switch (evt.getKeyChar()) {
-						case 'e': {
-								int x = rand.nextInt(350);
+                            case 'e': {
+                                	int x = rand.nextInt(350);
 		                        int y = rand.nextInt(350);
 		                        int w = rand.nextInt(50);
 		                        int h = rand.nextInt(50);
@@ -53,10 +69,11 @@ class ListFrame extends JFrame {
 		                        figs.add(e);
 		                        e.print();
 		                        repaint();  // outer.repaint()
+                                        //layeredPane.add(e);
 		                        break;
-							                   }
-						 case 'r':{
-								int x = rand.nextInt(350);
+                                        }
+                            case 'r':{
+					int x = rand.nextInt(350);
 		                        int y = rand.nextInt(350);
 		                        int w = rand.nextInt(50);
 		                        int h = rand.nextInt(50);
@@ -66,8 +83,9 @@ class ListFrame extends JFrame {
 		                        figs.add(r);
 		                        r.print();
 		                        repaint();  // outer.repaint()
+                                        //layeredPane.add(r);
 		                        break;
-							                   }
+                                    }
 		                  case 't':{
 		                        int x = rand.nextInt(350);
 		                        int y = rand.nextInt(350);
@@ -91,8 +109,9 @@ class ListFrame extends JFrame {
 		                        Trapezio c = new Trapezio(x,y, w ,h,fundo,contorno);                       
 		                        figs.add(c);
 		                        c.print();
-		                        repaint();  // outer.repaint()
-		                        break;      
+		                        repaint(); // outer.repaint()
+                                        break;     
+                                    
 		                 }
 				}
            }
@@ -100,39 +119,108 @@ class ListFrame extends JFrame {
       );
         this.addMouseListener(new MouseAdapter() {
              public void mousePressed (MouseEvent evt){
-            	 
+            	
             	 int x,y = 0;
             	 x= evt.getX();
               y= evt.getY();
-            	 for(Figure fig: figs) {
-            		 if((x >= fig.x) && (x<=((fig.x)+(fig.w))) && ((y>= fig.y) && (y<=(fig.y) + (fig.w)))){
-                      System.out.println(fig.getClass()); 
-                      focus= fig; 
-                 } 
-            	 }   
+            	for(Figure fig: figs) {
+                    if((x >= fig.x) && (x<=((fig.x)+(fig.w))) && ((y>= fig.y) && (y<=(fig.y) + (fig.w)))){ 
+                        foco = fig;
+                        Figure temp;
+                        int c = figs.indexOf(fig);
+                        int z = ((int)figs.size()-1);
+                        temp = figs.get(z);
+                        figs.set(z, figs.get(c));
+                        figs.set(c, temp);
+                
+                
+                       
+                       foco.setselected(true);
+                       repaint();
+                       
+                        
+                        System.out.println(fig.selected);
+                        
+                        
+                       
+                        }
+                    else{
+                        fig.setselected(false);
+                    }
+            	 }
+                
             			  
             	
              }
         });
 
-       /* this.addMouseMotionListener(new MouseMotionAdapter(){
+        this.addMouseMotionListener(new MouseMotionAdapter(){
               public void mouseDragged( MouseEvent evt){
-             
-              }
-*/
+                  
+                  
+                  
+                  for(Figure fig: figs) {
+                        if(fig.selected==true){
+                            
+                            Point p = MouseInfo.getPointerInfo().getLocation();
+                            int x = p.x - getLocation().x;
+                            int y = p.y - getLocation().y;
+                            System.out.format("%d x\n %d y\n ", fig.x,fig.y);
+                            fig.drag(x, y);
+                            System.out.format("%d x\n %d y\n ", fig.x,fig.y);
+                            break;
+                            
+                        }
+                        
+                        else{
+                                fig.setselected(false);
+                                
+                            } 
+                        
+                        
+                  }
+                  repaint();
+                  }
+              });
+
 
         this.setTitle("Lista de Elipses");
         this.setSize(350, 350);
+        
+       
+        
    }
    
 
 
     public void paint (Graphics g) {
+        
+        
+        
+       
         super.paint(g);
-        for (Figure fig: this.figs) {
-            fig.paint(g);
-            
+        for (Figure fig: figs) {
+            if(fig.selected == true){
+               
+              
+               int c = figs.indexOf(fig);
+               figs.get(c).paint(g);
+               figs.get(c).foculisedobj(g);
+               
+               
+                
+
+                
+            }
+            else{
+                fig.paint(g);
+                
+            }
         }
+            
+           
+        
+        
     }
 }
 
