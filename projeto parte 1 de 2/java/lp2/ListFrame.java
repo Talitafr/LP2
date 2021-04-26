200e876bc67351c625e4ccd6fdb982cb8f364fbe
Talita Ribeiro <57768101+Talitafr@users.noreply.github.com>
2021-04-25 22:16:10 -0300

    Add files via upload
    Tecla r - Gera um novo retângulo aleatório na posição do mouse;
    Tecla e - Gera uma nova elipse aleatória na posição do mouse;
    Tecla c - Gera um trapézio aleatório na posição do mouse;
    Tecla t - Gera um triângulo aleatório na posição do mouse;
    Tecla m - Quando a figura ta selecionada, abre a palheta de cores pra você mudar a cor de fundo;
    Tecla l (L minusculo) - Quando a figura ta selecionada, abre a palheta de cores pra você mudar a cor de contorno;
    Tecla Delete - Deleta a figura selecionada;
    Tecla + do Numpad - Aumenta a dimensão da figura selecionada;
    Tecla - do teclado principal - Diminui a dimensão da figura selecionada;
    Setas do teclado - Move a figura;
    Tecla TAB - Redireciona o foco para outra figura da lista de figuras;
    Mouse pode: Movimentar a figura pelo frame, selecionar uma figura ou,
    (se apertar em uma das teclas de Gerar figura) criar uma figura na posição do mouse.

--- projeto parte 1 de 2/java/lp2/ListFrame.java
+++ projeto parte 1 de 2/java/lp2/ListFrame.java
@@ -1,16 +1,16 @@
 
 /**
-*Tecla r - Gera um novo retângulo aleatório;
-*Tecla e - Gera uma nova elipse aleatória;
-*Tecla c - Gera um trapézio aleatório;
-*Tecla t - Gera um triângulo aleatório.
-*Tecla m - Quando a figura ta selecionada, abre a palheta de cores pra você mudar a cor de fundo
-*Tecla l (L minúsculo) - Quando a figura ta selecionada, abre a palheta de cores pra você mudar a cor de contorno
-*Tecla Delete - Deleta a figura selecionada
-*Tecla + do Numpad - Aumenta a dimensão da figura selecionada
-*Tecla - do teclado principal - Diminui a dimensão da figura selecionada
-*Setas do teclado - Move a figura
-*Tecla TAB - Redireciona o foco para outra figura da lista de figuras (aleatória)
+*Tecla r - Gera um novo retângulo aleatório na posição do mouse;
+*Tecla e - Gera uma nova elipse aleatória na posição do mouse;
+*Tecla c - Gera um trapézio aleatório na posição do mouse;
+*Tecla t - Gera um triângulo aleatório na posição do mouse;
+*Tecla m - Quando a figura ta selecionada, abre a palheta de cores pra você mudar a cor de fundo;
+*Tecla l (L minusculo) - Quando a figura ta selecionada, abre a palheta de cores pra você mudar a cor de contorno;
+*Tecla Delete - Deleta a figura selecionada;
+*Tecla + do Numpad - Aumenta a dimensão da figura selecionada;
+*Tecla - do teclado principal - Diminui a dimensão da figura selecionada;
+*Setas do teclado - Move a figura;
+*Tecla TAB - Redireciona o foco para outra figura da lista de figuras;
 *Mouse pode: Movimentar a figura pelo frame, selecionar uma figura ou,
 *(se apertar em uma das teclas de Gerar figura) criar uma figura na posição do mouse.
 */
@@ -117,7 +117,7 @@ class ListFrame extends JFrame {
                                      for(Figure fig: figs) {
                                          if(fig.selected==true){
                                              JColorChooser colorChooser = new JColorChooser();
-                                            fig.fundo = JColorChooser.showDialog(null, "escolha uma cor ", Color.blue);
+                                            fig.fundo = JColorChooser.showDialog(null, "Escolha uma cor para o fundo da figura", Color.black);
                                             repaint();
                                          }
                                      
@@ -126,11 +126,11 @@ class ListFrame extends JFrame {
                                  break;
                                  }
 
-                                    case 'l':{// L minusculo
+                                    case 'l':{
                                      for(Figure fig: figs) {
                                          if(fig.selected==true){
                                              JColorChooser colorChooser = new JColorChooser();
-                                            fig.contorno = JColorChooser.showDialog(null, "escolha uma cor ", Color.blue);
+                                            fig.contorno = JColorChooser.showDialog(null, "Escolha uma cor para o contorno da figura ", Color.black);
                                             repaint();
                                          }
                                      
